f9bcf225399d4d8f858e4c9529cd379ccfbc75f2
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-02-18 17:54:03 -0500

    0.0.19

--- be-deslotted.ts
+++ be-deslotted.ts
@@ -1,6 +1,5 @@
 import {define, BeDecoratedProps} from 'be-decorated/be-decorated.js';
 import {BeDeslottedVirtualProps, BeDeslottedProps, BeDeslottedActions} from './types';
-import {nudge} from 'trans-render/lib/nudge.js';
 import {register} from 'be-hive/register.js';
 
 export class BeDeslottedController implements BeDeslottedActions{
